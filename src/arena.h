cd68347dd9debcd8a943539829ecd1c5181acf74
olemorud <ole.kristian.morud@cern.ch>
2023-05-15 00:45:52 +0200

    Remove memcpy use

--- src/arena.h
+++ src/arena.h
@@ -2,18 +2,17 @@
 #ifndef ARENA_H
 #define ARENA_H
 
-
 #include <stddef.h> // ptrdiff_t
 
-struct arena {
+typedef struct arena {
     unsigned char *begin,
                   *next;
     ptrdiff_t cap;
-};
+} arena_t;
 
-struct arena* arena_new();
-void arena_reset(struct arena *a);
-void* arena_alloc(struct arena *a, size_t len);
+arena_t* arena_new();
+void arena_reset(arena_t *a);
+void* arena_alloc(arena_t *a, size_t len);
 
 
 #endif
